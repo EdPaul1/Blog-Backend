893d3d349961fed75c99e6475ce86fce8e705dd7
Paul <EdPaul1>
2024-04-29 22:18:41 +0300

    Implement Pagination, Canonical URL for Posts and SEO friendly URls

--- Blog/mysite/blog/views.py
+++ Blog/mysite/blog/views.py
@@ -1,20 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 # A view to display a list of posts
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Paginator to return 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        # deliver the last results if  range is out of number
+        posts = paginator.page(paginator.num_pages)
+
     return render(request,
                   'blog/post/list.html',
                   {'posts':posts})
 
 # A view to display a single post
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
-                                id=id,
-                                status=Post.Status.PUBLISHED)
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
