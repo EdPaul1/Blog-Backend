ed8300431728e110440903f780f6506433d3d68b
Paul <EdPaul1>
2024-06-24 12:14:22 +0300

    Change: Use a class based view to list posts.2 Creating forms in django 3. recommending posts be email

--- Blog/mysite/blog/views.py
+++ Blog/mysite/blog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import EmailPostForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from django.core.mail import send_mail
 
 # Create your views here.
+# Example of Class Based Views
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
 # A view to display a list of posts
 def post_list(request):
     post_list = Post.published.all()
@@ -33,3 +43,28 @@ def post_detail(request, year, month, day, post):
     return render(request,
                   'blog/post/detail.html',
                   {'post': post})
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
+    if request.method == 'POST':
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Form fields passed validation
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+            post.get_absolute_url())
+            subject = f"{cd['name']} recommends you read " \
+            f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" \
+            f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com',
+            [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+    'form': form,
+    'sent': sent})
